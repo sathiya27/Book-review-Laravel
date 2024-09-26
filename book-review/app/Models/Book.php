ecc5a4e03d94093b5c58629cdfbbca40f1eb0db6
SathiyaS <selan.sathiya16@gmail.com>
2024-09-26 19:18:56 +0800

    More local query scope for Book model

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -21,13 +21,35 @@ class Book extends Model
         return $query->where('title', 'Like', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query) : Builder
+    public function scopePopular(Builder $query, $from = null, $to = null) : Builder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+        return $query->withCount([
+            'reviews' => fn(Builder $q)=> $this->dateRangeFilter($from, $to, $q)
+        ])
+            ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query) : Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null) : Builder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($from, $to, $q)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReview(Builder $query, int $minReview)
+    {
+        return $query->having('reviews_count', '>=', $minReview);
+    }
+
+    private function dateRangeFilter($from, $to, Builder $q)
+    {
+        if($from && !$to){
+            $q->where('created_at', '>=', $from);
+        } elseif(!$from && $to){
+            $q->where('created_at', '<=', $to);
+        } elseif($from && $to){
+            $q->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
